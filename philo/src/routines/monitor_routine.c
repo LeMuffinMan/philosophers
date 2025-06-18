572aed05fe3983bfab016d32a9298ba04270e2e8
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-18 15:10:28 +0200

    parsing arguments done

--- philo/src/routines/monitor_routine.c
+++ philo/src/routines/monitor_routine.c
@@ -13,44 +13,44 @@
 #include "philo.h"
 #include <unistd.h>
 
-static int is_a_philo_dead(t_data *data)
-{
-
-  return (0);
-}
-
-static int did_philo_ate_enough(t_data *data)
-{
-  int i;
-  int count;
-
-  i = 0;
-  while (i < data->nb_philo)
-  {
-    pthread_mutex_unlock(&data->philosophers[i].mutex_nb_meals_eaten);
-    if (data->philosophers[i].nb_meals_eaten == data->number_of_times_each_philosopher_must_eat)
-      count++;
-  }
-  if (count == data->nb_philo)
-    return (1);
-  return (0);
-}
-
-void *monitor_routine(void *arg)
-{
-  t_data *data;
-
-  data = (t_data *)arg;
-  while (1)
-  {
-    if (is_a_philo_dead(data) || did_philo_ate_enough(data)) // besoin du mutex ?
-      break ;
-    usleep(1000);
-  }
-  pthread_mutex_lock(&data->mutex_end);
-  data->end = 1;
-  pthread_mutex_unlock(&data->mutex_end);
-  return (0);
-  //on check si un philo est mort
-  //on check le nombre de repas si on a un nombre de repas a faire
-}
+/* static int is_a_philo_dead(t_data *data) */
+/* { */
+/**/
+/*   return (0); */
+/* } */
+/**/
+/* static int did_philo_ate_enough(t_data *data) */
+/* { */
+/*   int i; */
+/*   int count; */
+/**/
+/*   i = 0; */
+/*   while (i < data->nb_philo) */
+/*   { */
+/*     pthread_mutex_unlock(&data->philosophers[i].mutex_nb_meals_eaten); */
+/*     if (data->philosophers[i].nb_meals_eaten == data->number_of_times_each_philosopher_must_eat) */
+/*       count++; */
+/*   } */
+/*   if (count == data->nb_philo) */
+/*     return (1); */
+/*   return (0); */
+/* } */
+/**/
+/* void *monitor_routine(void *arg) */
+/* { */
+/*   t_data *data; */
+/**/
+/*   data = (t_data *)arg; */
+/*   while (1) */
+/*   { */
+/*     if (is_a_philo_dead(data) || did_philo_ate_enough(data)) // besoin du mutex ? */
+/*       break ; */
+/*     usleep(1000); */
+/*   } */
+/*   pthread_mutex_lock(&data->mutex_end); */
+/*   data->end = 1; */
+/*   pthread_mutex_unlock(&data->mutex_end); */
+/*   return (0); */
+/*   //on check si un philo est mort */
+/*   //on check le nombre de repas si on a un nombre de repas a faire */
+/* } */
--- philo/src/routines/philosophers_routine.c
+++ philo/src/routines/philosophers_routine.c
@@ -13,24 +13,24 @@
 #include "philo.h"
 #include <stdio.h>
 //pas clair le arg
-void *philo_routine(void *arg)
-{    
-  t_philosopher *philosopher;
-
-  philosopher = (t_philosopher *)arg;
-  //mon mutex_start en dans ma struct data, et dans chaque philo j'ai un pointeur sur mutex_start : ca marche ?
-  pthread_mutex_lock(philosopher->mutex_start);
-  pthread_mutex_unlock(philosopher->mutex_start); 
-  printf("thread %d started\n", philosopher->id);
-  while (1)
-  {
-    printf("thinking\n");
-    printf("eating\n");
-    philosopher->nb_meals_eaten++;
-    printf("sleeping\n");
-    //mettre un break dans ce while ou join a la fin suffit ?
-  }
-  return (0);
-}
-
+/* void *philo_routine(void *arg) */
+/* {     */
+/*   t_philosopher *philosopher; */
+/**/
+/*   philosopher = (t_philosopher *)arg; */
+/*   //mon mutex_start en dans ma struct data, et dans chaque philo j'ai un pointeur sur mutex_start : ca marche ? */
+/*   pthread_mutex_lock(philosopher->mutex_start); */
+/*   pthread_mutex_unlock(philosopher->mutex_start);  */
+/*   printf("thread %d started\n", philosopher->id); */
+/*   while (1) */
+/*   { */
+/*     printf("thinking\n"); */
+/*     printf("eating\n"); */
+/*     philosopher->nb_meals_eaten++; */
+/*     printf("sleeping\n"); */
+/*     //mettre un break dans ce while ou join a la fin suffit ? */
+/*   } */
+/*   return (0); */
+/* } */
+/**/
 
