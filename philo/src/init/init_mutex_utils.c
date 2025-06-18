bdbbb128e9fd13290babde76ebc41beeb1738b67
oelleaum <oelleaum@student.42lyon.fr>
2025-06-18 17:59:55 +0200

    init reorganized

---
+++ philo/src/init/init_mutex_utils.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_mutex_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oelleaum <oelleaum@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 17:57:02 by oelleaum          #+#    #+#             */
+/*   Updated: 2025/06/18 17:59:32 by oelleaum         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int init_mutex_start_mutex(t_data **data)
+{
+  if (pthread_mutex_init(&(*data)->start_mutex, NULL) != 0)
+  {
+    destroy_forks_mutex(data, (*data)->nb_philo - 1);
+    return (print_error_and_free("Start mutex init failed\n", MUTEX_ERROR, data));
+  }
+  return (0);
+}
+
+int init_mutex_end_mutex(t_data **data)
+{
+  if (pthread_mutex_init(&(*data)->end_mutex, NULL) != 0)
+  {
+    destroy_forks_mutex(data, (*data)->nb_philo - 1);
+    pthread_mutex_destroy(&(*data)->start_mutex);
+    return (print_error_and_free("End mutex init failed\n", MUTEX_ERROR, data));
+  }
+  return (0);
+}
+
+int init_mutex_write_mutex(t_data **data)
+{
+  if (pthread_mutex_init(&(*data)->write_mutex, NULL) != 0)
+  {
+    destroy_forks_mutex(data, (*data)->nb_philo - 1);
+    pthread_mutex_destroy(&(*data)->start_mutex);
+    pthread_mutex_destroy(&(*data)->end_mutex);
+    return (print_error_and_free("Write mutex init failed\n", MUTEX_ERROR, data));
+  }
+  return (0);
+}
+
+int init_mutex_meals_limit_mutex(t_data **data)
+{
+  if (pthread_mutex_init(&(*data)->meals_limit_mutex, NULL) != 0)
+  {
+    destroy_forks_mutex(data, (*data)->nb_philo -1);
+    pthread_mutex_destroy(&(*data)->write_mutex);
+    pthread_mutex_destroy(&(*data)->start_mutex);
+    pthread_mutex_destroy(&(*data)->end_mutex);
+    return (print_error_and_free("meals_limit mutex init failed\n", MUTEX_ERROR, data));
+  }
+  return (0);
+}
