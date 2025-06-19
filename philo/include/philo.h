f6e8cbf59ddc71f98e646ae3a245f669fdf26ab8
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-19 18:47:42 +0200

    init_threads without leaks

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -13,93 +13,101 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stdbool.h>
-#include <pthread.h>
+# include <pthread.h>
+# include <stdbool.h>
 
 # define INVALID_ARG 1
 # define MALLOC_ERROR -1
 # define MUTEX_ERROR -2
 # define THREAD_ERROR -3
 
-// typedef struct s_philosopher
-// {
-//   int id;
-//   int *time_start;
-//   int last_meal;
-//   pthread_mutex_t mutex_nb_meals_eaten;
-//   pthread_mutex_t *mutex_start;
-//   int nb_meals_eaten;
-//   pthread_t thread; 
-// } t_philosopher;
+typedef struct s_philosopher
+{
+  int id;
+  // int *time_start;
+  pthread_mutex_t last_meal_mutex;
+  int last_meal;
+  pthread_mutex_t nb_meals_eaten_mutex;
+  int nb_meals_eaten;
+  pthread_t *threads;
+  pthread_mutex_t *mutex_start;
+  bool *start;
+  pthread_mutex_t *mutex_end;
+} t_philosopher;
 
 typedef struct s_data
 {
-  int nb_philo;
-  int time_to_die;
-  int time_to_eat;
-  int time_to_sleep;
-  bool *forks;
-  pthread_mutex_t *forks_mutex;
-  pthread_mutex_t start_mutex;
-  pthread_mutex_t end_mutex;
-  bool end; // pour le deces
-  pthread_mutex_t write_mutex;
-  pthread_mutex_t meals_limit_mutex;
-  int meals_limit;
-  // t_philosopher *philosophers;
-  // pthread_t monitor;
-  // int time_start;
-  // pthread_mutex_t mutex_end;
-} t_data;
-
+	int				nb_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	bool			*forks;
+	pthread_mutex_t	*forks_mutex;
+	pthread_mutex_t	start_mutex;
+	bool			start;
+	pthread_mutex_t	end_mutex;
+	bool			end;
+	pthread_mutex_t	write_mutex;
+	pthread_mutex_t	meals_limit_mutex;
+	int				meals_limit;
+	pthread_t		*threads;
+	pthread_t monitor;
+	t_philosopher *philosophers;
+	// pthread_t monitor;
+	// int time_start;
+	// pthread_mutex_t mutex_end;
+}					t_data;
 
 typedef enum e_type
 {
-  FORK,
-  EAT,
-  SLEEP,
-  THINK,
-  DIE
-} t_type;
+	FORK,
+	EAT,
+	SLEEP,
+	THINK,
+	DIE
+}					t_type;
 
-//init.c
-int init_data(t_data **data, char **av);
-int init_mutex(t_data **data);
+// init.c
+int					init_data(t_data **data, char **av);
+int					init_mutex(t_data **data);
+int init_threads(t_data **data);
 // int init_forks(t_data *data);
-// int init_threads(t_data *data);
 
-//init_mutex_utils
-int init_mutex_start_mutex(t_data **data);
-int init_mutex_end_mutex(t_data **data);
-int init_mutex_write_mutex(t_data **data);
-int init_mutex_meals_limit_mutex(t_data **data);
-int destroy_forks_mutex(t_data **data, int last_mutex);
+// init_mutex_utils
+int					init_mutex_start_mutex(t_data **data);
+int					init_mutex_end_mutex(t_data **data);
+int					init_mutex_write_mutex(t_data **data);
+int					init_mutex_meals_limit_mutex(t_data **data);
+int					destroy_forks_mutex(t_data **data, int last_mutex);
 
-//utils 
-  //prints
-int print_error_and_free(char *msg, int exit_code, t_data **data);
+// utils
+// prints
+int					print_error_and_free(char *msg, int exit_code,
+						t_data **data);
 // int print_update(int time_elapsed, int philo, t_type type);
 // int print_error(char *input, char *arg_type, t_data *data);
 // int print_update(int time_elapsed, int philo, t_type type);
 
-  //str_utils
-int are_valids_args(char **av);
-int ft_atoi(const char *nptr);
+// str_utils
+int					are_valids_args(char **av);
+int					ft_atoi(const char *nptr);
 // char *ft_itoa(int n);
 // int ft_strlen(char *str);
 // int copy_chars(char **dest, char *src, int dest_i, int src_i);
 // char *ft_strjoin3(char *time_elapsed, char *philo, char *action);
 // int ft_atoi(char *str);
 
-  //free_fcts.c
-int free_allocated_memory(t_data **data);
+// free_fcts.c
+int					free_allocated_memory(t_data **data);
+int simulation_end_destroy_and_free(t_data **data);
+int join_threads(t_data **data, int i);
+int destroy_all_philo_mutex(t_data **data, int i);
 // int destroy_mutex_free_exit(t_data *data, pthread_mutex_t *fork_mutex, int i, int exit_code);
 
-//routines
-  //monitor_routine
-// void *monitor_routine(void *arg);
-  //philosophers_routine
-// void *philo_routine(void *arg);
+// routines
+// monitor_routine
+void *monitor_routine(void *arg);
+// philosophers_routine
+void *philosophers_routine(void *arg);
 
-#endif 
-
+#endif
