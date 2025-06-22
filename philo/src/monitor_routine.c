95f0cf3c45b5202370723c0a9e962a606b616e8a
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-22 02:16:22 +0200

    implementing end

--- philo/src/monitor_routine.c
+++ philo/src/monitor_routine.c
@@ -2,6 +2,33 @@
 #include <pthread.h>
 #include "philo.h"
 #include <unistd.h> //usleep
+#include <stdio.h> // a virerr !!!!
+
+bool get_fed_state(t_philosopher *philosopher)
+{
+  bool state;
+
+  pthread_mutex_lock(&philosopher->fed_mutex);
+  state = philosopher->fed;
+  pthread_mutex_unlock(&philosopher->fed_mutex);
+  return (state);
+}
+
+long int get_last_meal_time(t_philosopher *philosopher)
+{
+  long int last_meal_time;
+  pthread_mutex_lock(&philosopher->last_meal_mutex);
+  last_meal_time = philosopher->last_meal;
+  pthread_mutex_unlock(&philosopher->last_meal_mutex);
+  return (last_meal_time);
+}
+
+int set_end(t_data **data, pthread_mutex_t *mutex)
+{
+  pthread_mutex_lock(mutex);
+  (*data)->end = true;
+  pthread_mutex_unlock(mutex);
+}
 
 bool are_philo_fed(t_data **data)
 {
@@ -10,34 +37,72 @@ bool are_philo_fed(t_data **data)
   i = 0;
   while (i < (*data)->nb_philo)
   {
-    pthread_mutex_lock(&(*data)->philosophers[i].fed_mutex);
-    if ((*data)->philosophers[i].fed == false)
-    {
-      pthread_mutex_unlock(&(*data)->philosophers[i].fed_mutex);
+    if (!get_fed_state(&(*data)->philosophers[i]))
       return (false);
-    }
-    pthread_mutex_unlock(&(*data)->philosophers[i].fed_mutex);
     i++;
   }
+  set_end(data, &(*data)->end_mutex);
   return (true);
+}
+
+int are_philo_starving(t_data **data)
+{
+  int i;
+  long int time_elapsed;
+  long int last_meal_time;
+
+  i = 0;
+  while (i < (*data)->nb_philo)
+  {
+    last_meal_time = get_last_meal_time(&(*data)->philosophers[i]);
+    pthread_mutex_lock(&(*data)->time_mutex);
+    time_elapsed = get_time() - (*data)->start_time - last_meal_time;
+    pthread_mutex_unlock(&(*data)->time_mutex);
+    pthread_mutex_lock(&(*data)->write_mutex);
+    printf("time elapsed : %ld\n", time_elapsed);
+    pthread_mutex_unlock(&(*data)->write_mutex);
+    if (last_meal_time > 0 && time_elapsed > (*data)->time_to_die)
+    {
+      print_log(&(*data)->philosophers[i], DIE);
+      set_end(data, &(*data)->end_mutex);
+      return (true);
+    }
+    i++;
+  }
+  return (false);
 }
 
 int main_thread_monitoring(t_data **data) // a bouger !
 {
-  while (1)
-  {
-    if (are_philo_fed(data))
-    {
-      pthread_mutex_lock(&(*data)->end_mutex);
-      (*data)->end = true;
-      pthread_mutex_unlock(&(*data)->end_mutex);
-      return (1);
-    }
-    accurate_sleep (1000);
+  int exit_code;
+
+  exit_code = false;
   /* pthread_mutex_lock(&(*data)->write_mutex); */
   /* printf("monitor started at %ld\n", get_time() - (*data)->start_time); */
   /* pthread_mutex_unlock(&(*data)->write_mutex); */
-    usleep(100);
+  while (1);
+  {
+    //avec un temps de mort tres court, les threads partent quand meme !
+    exit_code = are_philo_fed(data);
+    //LE PB EST ICI !!
+  pthread_mutex_lock(&(*data)->write_mutex);
+  printf("fed exit_code = %d\n", exit_code);
+  pthread_mutex_unlock(&(*data)->write_mutex);
+    if (exit_code)
+      return (exit_code);
+    exit_code = are_philo_starving(data);
+  pthread_mutex_lock(&(*data)->write_mutex);
+  printf("fed exit_code = %d\n", exit_code);
+  pthread_mutex_unlock(&(*data)->write_mutex);
+    if (exit_code)
+      return (exit_code);
+    /* if (are_philo_fed(data) || are_philo_starving(data)) */
+    /* { */
+    /*   set_end((*data)->end_mutex); */
+    /*   return (1); */
+    /* } */
+    accurate_sleep (1000);
+    /* usleep(100); */
   }
   return (0);
 }
