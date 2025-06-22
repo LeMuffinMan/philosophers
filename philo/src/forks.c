f0d0b9243060e9b03c238b34a44f2846b0484d05
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-23 00:01:03 +0200

    fixed stuck at death

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -20,12 +20,12 @@ bool get_fork_state(t_philosopher *philosopher, int i)
   return (exit_code);
 }
 
-int try_to_catch_fork(t_philosopher *philosopher, int fork_to_catch)
+bool try_to_catch_fork(t_philosopher *philosopher, int fork_to_catch)
 {
-  int exit_code;
+  /* bool exit_code; */
 
-  exit_code = get_fork_state(philosopher, fork_to_catch);
-  while (!exit_code)
+  /* exit_code = get_fork_state(philosopher, fork_to_catch); */
+  while (!get_fork_state(philosopher, fork_to_catch))
   {
     if (is_simulation_over(philosopher))
       return (false);
@@ -33,9 +33,10 @@ int try_to_catch_fork(t_philosopher *philosopher, int fork_to_catch)
     /* printf("trying to catch fork %d\n", fork_to_catch); */
     /* pthread_mutex_unlock(&philosopher->data->write_mutex); */
     usleep(100);
-    exit_code = get_fork_state(philosopher, fork_to_catch);
+    if (is_simulation_over(philosopher))
+      return (false);
   }
-  return (exit_code);
+  return (true);
 }
 
 int set_fork(t_philosopher *philosopher, int fork, bool state)
@@ -85,8 +86,8 @@ int release_forks(t_philosopher *philosopher)
 
   left = philosopher->id;
   right = (philosopher->id + 1) % philosopher->nb_philo;
-  /* if (is_simulation_over(philosopher)) */
-  /*   return (1); */
+  if (is_simulation_over(philosopher))
+    return (1);
   if (left > right)
   {
     set_fork(philosopher, left, true);
@@ -97,7 +98,7 @@ int release_forks(t_philosopher *philosopher)
     set_fork(philosopher, right, true);
     set_fork(philosopher, left, true);
   }
-  /* if (is_simulation_over(philosopher)) */
-  /*   return (1); */
+  if (is_simulation_over(philosopher))
+    return (1);
   return (0);
 }
