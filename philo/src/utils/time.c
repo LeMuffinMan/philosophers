2d8023fe4dc469f53f014a6325684669f1dc1ced
oelleaum <oelleaum@student.42lyon.fr>
2025-06-23 15:42:16 +0200

    utils normed, time.c gettimeofday error

--- philo/src/utils/time.c
+++ philo/src/utils/time.c
@@ -1,53 +1,50 @@
 
-#include <pthread.h>
 #include "philo.h"
-#include <sys/time.h>
+#include <pthread.h>
 #include <stddef.h> // POUR NULL !!
-#include <unistd.h> //usleep
 #include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h> //usleep
 
-//returns ms elapsed since 1 january 1970
-long int get_time(void)
+// returns ms elapsed since 1 january 1970
+long int	get_time(void)
 {
-  struct timeval tv;
-  
-  gettimeofday(&tv, NULL);
-  //gerer l'erreur du gettimeofday ? 
-  return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000)); 
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	// gerer l'erreur du gettimeofday ?
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
 }
-bool is_simulation_over(t_philosopher *philosopher)
+bool	is_simulation_over(t_philosopher *philosopher)
 {
-  pthread_mutex_lock(&philosopher->data->end_mutex);
-  if (philosopher->data->end)
-  {
-    /* pthread_mutex_lock(&philosopher->data->write_mutex); */
-    /* printf("id leaving %d\n", philosopher->id); */
-    /* pthread_mutex_unlock(&philosopher->data->write_mutex); */
-    pthread_mutex_unlock(&philosopher->data->end_mutex);
-    return (true);
-  }
-  pthread_mutex_unlock(&philosopher->data->end_mutex);
-  return (false);
-}
-
-long int is_time_started(t_philosopher *philosopher)
-{
-  long int start_time;
-
-  start_time = false;
-  pthread_mutex_lock(&philosopher->data->time_mutex); 
-  if (philosopher->data->start_time != -1)
-    start_time = philosopher->data->start_time;
-  pthread_mutex_unlock(&philosopher->data->time_mutex); 
-  return (start_time);
+	pthread_mutex_lock(&philosopher->data->end_mutex);
+	if (philosopher->data->end)
+	{
+		pthread_mutex_unlock(&philosopher->data->end_mutex);
+		return (true);
+	}
+	pthread_mutex_unlock(&philosopher->data->end_mutex);
+	return (false);
 }
 
-int accurate_sleep(int time_to_sleep)
+long int	is_time_started(t_philosopher *philosopher)
 {
-  long int start_time;
+	long int	start_time;
 
-  start_time = get_time();
-  while ((get_time() - start_time) < time_to_sleep)
-    usleep(100); 
-  return (0);
+	start_time = false;
+	pthread_mutex_lock(&philosopher->data->time_mutex);
+	if (philosopher->data->start_time != -1)
+		start_time = philosopher->data->start_time;
+	pthread_mutex_unlock(&philosopher->data->time_mutex);
+	return (start_time);
+}
+
+int	accurate_sleep(int time_to_sleep)
+{
+	long int	start_time;
+
+	start_time = get_time();
+	while ((get_time() - start_time) < time_to_sleep)
+		usleep(100);
+	return (0);
 }
