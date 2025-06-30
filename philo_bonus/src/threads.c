8ee55b8fc1aabd1f36ab222004898eed7b65acd6
Olivier Elleaume <162384047+LeMuffinMan@users.noreply.github.com>
2025-06-30 16:36:02 +0200

    one philo delayed deaath fixed

--- philo_bonus/src/threads.c
+++ philo_bonus/src/threads.c
@@ -2,6 +2,8 @@
 #include "philo_bonus.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void *philo_monitor_thread(void *args)
 {
@@ -9,11 +11,45 @@ void *philo_monitor_thread(void *args)
 
   simulation = (t_simulation *)args;
   sem_wait(simulation->sems.simulation_end);
+  /* printf("%d knows someone died\n", simulation->data.id); */
   sem_wait(simulation->sems.proc_end);
   /* printf("%d knows simulation is over\n", simulation->data.id); */
   simulation->data.end = true;
   sem_post(simulation->sems.proc_end);
-  sem_post(simulation->sems.simulation_end);
+  /* sem_post(simulation->sems.simulation_end); */
+  return (0);
+}
+
+int print_death(t_simulation *simulation, int id)
+{
+  sem_wait(simulation->sems.print);
+  printf("%ld %d died\n", get_time() - simulation->data.time.start, id);
+  sem_post(simulation->sems.fed);
+  sem_post(simulation->sems.print);
+  return (0);
+}
+
+int wait_dead_child(t_simulation *simulation)
+{
+  int status;
+  pid_t pid;
+  int i;
+
+  while (1)
+  {
+    pid = wait(&status);
+    if (pid > 0)
+    {
+      i = 0;
+      while (i < simulation->data.nb_philos)
+      {
+        /* printf("pid = %d | philo[%d] = %d\n", pid, i, simulation->philos[i]); */
+        if (pid == simulation->philos[i])
+          return (print_death(simulation, i));
+        i++;
+      }
+    }
+  }
   return (0);
 }
 
@@ -24,6 +60,14 @@ void *simulation_death_monitor_thread(void *args)
 
   simulation = (t_simulation *)args;
   sem_wait(simulation->sems.death);
+  /* printf("soneone died !\n"); */
+  i = 0;
+  while (i < simulation->data.nb_philos)
+  {
+    sem_post(simulation->sems.simulation_end); 
+    i++;
+  }
+  wait_dead_child(simulation);
   i = 0;
   while (i < simulation->data.nb_philos)
   {
